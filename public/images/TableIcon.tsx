01529e390fac2c84976ed207df235ca1b005c43c
KimWooHyun <ktkwhms3@gmail.com>
2024-04-15 20:04:27 +0900

    Fix: icon 속성값 jsx 형식으로

--- public/images/TableIcon.tsx
+++ public/images/TableIcon.tsx
@@ -9,8 +9,8 @@ export default function TableIcon() {
       className="h-full w-full"
     >
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M0 2C0 0.895431 0.636548 0 1.42177 0H834.578C835.363 0 836 0.895431 836 2V9V10V64H827.469V12H8.53072L8.53072 64H0.000106183L0.00010786 10.0249L0 10V2Z"
         fill="white"
       />
