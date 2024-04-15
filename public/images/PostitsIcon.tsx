40d04cc8205255432332877304e6597411a5ba7d
KimWooHyun <ktkwhms3@gmail.com>
2024-04-15 19:48:24 +0900

    Feat: svg파일 icon 컴포넌트로 수정

---
+++ public/images/PostitsIcon.tsx
@@ -0,0 +1,18 @@
+export default function PostitsIcon() {
+  return (
+    <svg
+      width="97"
+      height="95"
+      viewBox="0 0 97 95"
+      fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect x="12" y="59" width="16" height="22" />
+      <rect x="22" y="62" width="16" height="22" />
+      <rect y="22" width="16" height="22" />
+      <rect x="23" width="16" height="22" />
+      <rect x="51" y="33" width="16" height="22" />
+      <rect x="81" y="73" width="16" height="22" />
+    </svg>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,56 +1,30 @@
-import Image from 'next/image';
+import BooksIcon from '@images/BooksIcon';
+import CdsIcon from '@images/CdsIcon';
+import MacIcon from '@images/MacIcon';
+import PostitsIcon from '@images/PostitsIcon';
+import TableIcon from '@images/TableIcon';
 import React from 'react';
 
 export default async function Page() {
   return (
     <section className="flex h-screen items-center justify-center p-[5rem]">
       <div className="flex flex-col">
-        <div className="mb-5 flex h-full w-full justify-end pr-6">
-          <Image
-            src={'/images/postits.svg'}
-            width={100}
-            height={100}
-            alt=""
-            className="object-fill"
-          />
+        <div className="mb-5 flex h-full justify-end pr-6 text-white">
+          <PostitsIcon />
         </div>
-        <div className="relative flex h-full w-[550px] items-end justify-center gap-3">
-          <div className="h-full">
-            <Image
-              src={'/images/books.svg'}
-              width={170}
-              height={100}
-              alt=""
-              className="object-fill"
-            />
+        <div className="flex h-full w-[550px] items-end justify-center gap-3">
+          <div className="h-full w-[170px] text-white">
+            <BooksIcon />
           </div>
-          <div className="h-full">
-            <Image
-              src={'/images/mac.svg'}
-              width={180}
-              height={100}
-              alt=""
-              className="object-fill"
-            />
+          <div className="h-full w-[180px] text-white ">
+            <MacIcon />
           </div>
-          <div className="h-full">
-            <Image
-              src={'/images/cds.svg'}
-              width={75}
-              height={100}
-              alt=""
-              className="object-fill"
-            />
+          <div className="h-full w-[75px] text-white ">
+            <CdsIcon />
           </div>
         </div>
-        <div className="flex w-full justify-center">
-          <Image
-            src={'/images/table.svg'}
-            width={550}
-            height={100}
-            alt=""
-            className="object-fill"
-          />
+        <div className="flex max-w-[550px] justify-center text-white">
+          <TableIcon />
         </div>
       </div>
     </section>
